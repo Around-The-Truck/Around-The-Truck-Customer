3502bfac76dc4d1c8e823b2b8f80ff6e6ca718f7
ebsud89 <ebsud89@gmail.com>
2014-12-19 19:51:44 +0900

    sumin's modification

--- app/src/main/java/kr/co/aroundthetruck/customer/Brand.java
+++ app/src/main/java/kr/co/aroundthetruck/customer/Brand.java
@@ -10,6 +10,17 @@ public class Brand {
     private int like;
     private String category;
 
+    Brand(int brandImage,String brandName, String brandDistance, int like, String category){
+
+        this.brandImage = brandImage;
+        this.brandName= brandName;
+        this.brandDistance = brandDistance;
+        this.like = like;
+        this.category = category;
+
+
+    }
+
     public int getBrandImage(){ return brandImage;}
     public String getBrandName(){
         return brandName;
